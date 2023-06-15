97f2a501ac441c0dce7f1a8391393e7b944e57bb
Paribesh Chaudhary <prashantchaudhary@Paribeshs-MacBook-Pro.local>
2023-06-14 17:06:05 -0700

    Create Folder and File

---
+++ 8.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Creating folder and writing in file
+mkdir hello
+touch "hello/world.txt"
+echo "Hello World is inside the text file" >> "hello/world.txt"
+echo "Created hello/world.txt"
